20111f12b2be11272a026fa74e8bf0f571a24af0
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-10-27 13:04:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,36 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+$conn =new mysqli ("127.0.0.1", "root", "","nauka" );
+   $result=$conn ->query ('SELECT * from pracownicy');
+	   echo ("<table border =1>");
+	   echo ( "<th> id</th>");
+	   echo ("<th> imie </th>");
+	   echo ("<th> dzial </th>");
+	   echo ("<th> zarobki </th>");
+   while ($row=$result ->fetch_assoc()){
+	   echo("<tr>");
+	   echo ("<td>" .$row["id_pracownicy"]."</td>
+	   <td>".$row ["imie"]."</td>
+	   <td>" .$row ["dzial"]. "</td>
+	   <td>" .$row ["zarobki"]. "</td>");
+	   echo ("</tr>");
+}
+   echo ("</table>");
+ 
+   $result =$conn ->query ('SELECT* from pracownicy where imie  like "%a"');
+	   echo ("<table border>");
+	   echo ("<th> id </th>");
+	   echo ("<th> imie </th>" );
+	   echo ("<th> dzial </th>" );
+	   echo ("<th> zarobki</th>");
+	   while ($row=$result ->fetch_assoc())
+	   {;
+	   echo("<tr>");
+	   echo("<td>".$row ["id_pracownicy"]."</td>
+	   <td>".$row ["imie"]."</td>
+	   <td>".$row ["dzial"]."</td>
+	   <td>".$row ["zarobki"]."</td>");
+	   echo("</tr>");
+	   }
+	   echo ("</table>");
 ?>
-Something is wrong with the XAMPP installation :-(
+
